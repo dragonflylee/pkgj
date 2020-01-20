898cab979ec2cc050f3f32789da81d4695e27c50
dragonflylee <dragonflylee@outlook.com>
2020-01-20 13:26:09 +0800

    改用freetype加载字体

--- cross.cmake
+++ cross.cmake
@@ -91,6 +91,7 @@ target_link_libraries(pkgj
   jpeg
   z
   m
+  freetype
   SceAppMgr_stub
   SceAppUtil_stub
   SceCommonDialog_stub
@@ -99,8 +100,7 @@ target_link_libraries(pkgj
   SceGxm_stub
   SceHttp_stub
   SceNet_stub
-  SceNetCtl_stub
-  ScePvf_stub
+  SceNetCtl_stub  
   ScePower_stub
   ScePromoterUtil_stub
   SceShellSvc_stub
